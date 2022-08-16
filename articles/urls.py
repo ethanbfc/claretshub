a8a13f3788ecceb5b4f0b8d34402dfc072d5a79a
ethanbfc <ethanbfc@gmail.com>
2022-08-16 14:33:09 +0100

    feat: Implemented delete article functionality

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView, CreateArticleView, EditArticleView
+from .views import HomeView, ArticleDetailView, CreateArticleView, EditArticleView, DeleteArticleView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name="article-details"),
     path('create_article', CreateArticleView.as_view(), name="create-article"),
-    path('article/edit/<int:pk>', EditArticleView.as_view(), name="edit-article")
+    path('article/edit/<int:pk>', EditArticleView.as_view(), name="edit-article"),
+    path('article/delete/<int:pk>', DeleteArticleView.as_view(), name="delete-article"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,10 +1,12 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Article
 from .forms import CreateForm, EditForm
 
 class HomeView(ListView):
     model = Article
     template_name = 'index.html'
+    ordering = ['-id']
 
 class ArticleDetailView(DetailView):
     model = Article
@@ -19,3 +21,8 @@ class EditArticleView(UpdateView):
     model = Article
     form_class = EditForm
     template_name = 'edit_article.html'
+
+class DeleteArticleView(DeleteView):
+    model = Article
+    template_name = 'delete_article.html'
+    success_url = reverse_lazy('home')
