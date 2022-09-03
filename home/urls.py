482060e770cd9abd5f155b81136bca4f484d5d68
ethanbfc <ethanbfc@gmail.com>
2022-09-03 11:05:10 +0100

    feat: Basic pages for footer links

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home")
+    path('', HomeView.as_view(), name="home"),
+    path('about/', AboutView.as_view(), name="about"),
+    path('jobs/', JobView.as_view(), name="jobs"),
+    path('terms-of-service/', TermsOfServiceView.as_view(), name="terms-of-service"),
+    path('privacy-policy/', PrivacyPolicyView.as_view(), name="privacy-policy"),
+    path('cookie-policy/', CookiePolicyView.as_view(), name="cookie-policy"),
+    path('accessibility/', AccessibilityView.as_view(), name="accessibility"),
+    path('advertising/', AdvertisingView.as_view(), name="advertising")
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 from articles.models import Article
 
 class HomeView(ListView):
@@ -6,3 +6,24 @@ class HomeView(ListView):
     template_name = 'index.html'
     ordering = ['-date']
     paginate_by = 10
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class JobView(TemplateView):
+    template_name = 'jobs.html'
+
+class TermsOfServiceView(TemplateView):
+    template_name = 'terms_of_service.html'
+
+class PrivacyPolicyView(TemplateView):
+    template_name = 'privacy_policy.html'
+
+class CookiePolicyView(TemplateView):
+    template_name = 'cookie_policy.html'
+
+class AccessibilityView(TemplateView):
+    template_name = 'accessibility.html'
+
+class AdvertisingView(TemplateView):
+    template_name = 'advertising.html'
