e35ba06a3cacfdfb5fe6cd243d6cd7420b7db460
ethanbfc <ethanbfc@gmail.com>
2022-09-19 13:41:09 +0100

    feat: Add categories to contact form

--- home/forms.py
+++ home/forms.py
@@ -3,4 +3,6 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    categories = [('Bug Report', 'Bug Report'), ('Question', 'Question'), ('Sponsor', 'Sponsor Clarets Hub'), ('Privacy / Data Request', 'Privacy / Data Request'), ('Other', 'Other')]
+    category = forms.ChoiceField(choices=categories, widget=forms.Select(attrs={'class': 'form-control'}))
     message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
